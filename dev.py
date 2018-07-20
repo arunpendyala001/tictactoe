29da28ec996146f672daa0696ec8163976c735fb
arunpendyala001 <arunpendyala001@gmail.com>
2018-07-20 12:56:36 +0530

    i have added

---
+++ dev.py
@@ -0,0 +1,17 @@
+#development tools
+# -->pip
+# used as a package manager
+# used to install and uninstall
+# search, freeze and build the package
+
+
+
+
+
+# -->virtual environment
+# is a python package which is used to create isolated python projects
+
+
+
+
+# -->git
